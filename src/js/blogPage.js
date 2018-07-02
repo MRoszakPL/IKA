3eb76ae24a1a84897696cc45e8e9e5809e36ab2a
MRoszakPl <roszak.maciej.msg@gmail.com>
2018-07-02 16:34:52 +0200

    Changes in blog layout

--- src/js/blogPage.js
+++ src/js/blogPage.js
@@ -29,16 +29,19 @@ class LeftImageNews extends Component{
     render() {
         return (
 
+
+            <NavLink to={'/blogPost'}>
                 <div className={'row blogPost'}>
                     <div className={'col-md-8'}>
                         <h1>Lorem ipsum dolor sit amet</h1>
+                        <p>Data: 27.05.2018</p>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi et eveniet facere facilis iste quo ullam? Autem beatae deleniti iusto libero nam nulla odit quam suscipit unde vero!</p>
                     </div>
-                    <div className={'col-md-4'}>
-                        <img src={'./images/master.jpg'}/>
+                    <div style={{backgroundImage: "url('./images/Info.png')", backgroundPosition: 'center', backgroundSize: 'contain', backgroundRepeat: 'no-repeat'}} className={'col-md-4'}>
+
                     </div>
                 </div>
-
+            </NavLink>
         );
     }
 
@@ -48,17 +51,18 @@ class RightImageNews extends Component{
 
     render() {
         return (
-
+            <NavLink to={'/blogPost'}>
                 <div className={'row blogPost'}>
-                    <div className={'col-md-4'}>
-                        <img src={'./images/master.jpg'}/>
+                    <div style={{backgroundImage: "url('./images/Info.png')", backgroundPosition: 'center', backgroundSize: 'contain', backgroundRepeat: 'no-repeat'}} className={'col-md-4'}>
+
                     </div>
                     <div className={'col-md-8'}>
                         <h1>Lorem ipsum dolor sit amett</h1>
+                        <p>Data: 27.05.2018</p>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi et eveniet facere facilis iste quo ullam? Autem beatae deleniti iusto libero nam nulla odit quam suscipit unde vero!</p>
                     </div>
                 </div>
-
+            </NavLink>
         );
     }
 
@@ -68,15 +72,17 @@ class MainNews extends Component{
 
     render() {
         return (
-            <div className={'row mainNews'}>
-                <div className={' container'}>
-                    <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h1>
-                    <p>Data: 27.05.2018</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid autem dignissimos error
-                        officia officiis reiciendis. A accusantium animi, architecto commodi dolorum ea eum hic, magnam
-                        minima possimus quaerat ratione.</p>
+            <NavLink to={'/blogPost'}>
+                <div className={'row mainNews'}>
+                    <div className={' container'}>
+                        <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h1>
+                        <p>Data: 27.05.2018</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid autem dignissimos error
+                            officia officiis reiciendis. A accusantium animi, architecto commodi dolorum ea eum hic, magnam
+                            minima possimus quaerat ratione.</p>
+                    </div>
                 </div>
-            </div>
+            </NavLink>
         );
     }
 
